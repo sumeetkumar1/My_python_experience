5f512f02fc0532a00cb4da0280e7eb2abb012a12
sumeetkumar1 <sumeet.jakson@gmail.com>
2020-03-01 00:14:31 +0530

    added some changes to SQlite_1.py

--- P3/SQlite_1.py
+++ P3/SQlite_1.py
@@ -1,3 +1,8 @@
+import sqlite3
+conn = sqlite3.connect('org_count.sqlite')
+cur = conn.cursor()
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
 fn = input("Enter File name: ")
 fh = open(fn)
 
@@ -7,5 +12,20 @@ for line in fh:
         email = lines[1]
         doms = email.split('@')
         org = doms[1]
-        print(org)
-l
+        cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+        row = cur.fetchone()
+    if row is None:
+        cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (org,))
+    else
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
+    conn.commit()
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+cur.close()
+
+
+
+
