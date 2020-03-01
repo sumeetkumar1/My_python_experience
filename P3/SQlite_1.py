c3c0e81ff71e34663a87b325f91228d216e7094a
sumeetkumar1 <sumeet.jakson@gmail.com>
2020-03-01 14:16:51 +0530

    Re-structured directory

--- P3/SQlite_1.py
+++ P3/SQlite_1.py
@@ -1,5 +1,5 @@
 import sqlite3
-conn = sqlite3.connect('org_count.sqlite')
+conn = sqlite3.connect('orgs.sqlite')
 cur = conn.cursor()
 cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
 
@@ -14,10 +14,10 @@ for line in fh:
         org = doms[1]
         cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
         row = cur.fetchone()
-    if row is None:
-        cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (org,))
-    else
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
+        if row is None:
+            cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (org,))
+        else:
+            cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
     conn.commit()
 
 sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
@@ -25,6 +25,7 @@ sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
 cur.close()
+
 
 
 
